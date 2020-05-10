501be9dc7effd4d54263f1821b88ffbe2cffc43b
RazanAl-Quran <r.alquran@ltuc.com>
2020-05-10 15:00:33 +0300

    class01 demo

---
+++ class-01/demo/js/app.js
@@ -0,0 +1,6 @@
+var userName = prompt('what is your name?');
+console.log('username is = ',userName);
+alert('your name is '+ userName);
+var color = prompt('what is your favorite color?');
+
+alert('Hi there, '+ userName + ' your fav color is '+ color);
