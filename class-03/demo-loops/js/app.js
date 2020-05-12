1d84e8c8264d8b2cdc198fee2f950a97650e49fc
RazanAl-Quran <r.alquran@ltuc.com>
2020-05-12 14:24:44 +0300

    class03 demo

---
+++ class-03/demo-loops/js/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+//print numbers from 0-9 using for loop
+// for(var i=0; i <= 9; i++){
+//   console.log('I am at number '+i);
+// }
+
+// how is it executed
+// 1. variable is assigned, only once -> i = 0 
+// 2. condition is checked, if not met (executes to false) exit the loop, if met 
+// 3. execute the code inside the loop
+// 4. update the counter value
+// repeat 2, 3, 4
+
+//print numbers from 9-0 using for loop
+// for(var i=9; i>=0; i--){
+//   console.log('I am at number '+i);
+// }
+
+
+//looping over an array with for
+// var donutsFlavors = ['eclair', 'chocolate froasted', 'double chocolate', 'sugar dipped','honey dipped'];
+
+// for(var i=0; i< donutsFlavors.length;i++){
+//   console.log(i+1 +' '+ donutsFlavors[i]);
+// }
+
+
+//adding 2 user input items to array usinf for loop
+// var guessedName;
+// var namesArray = ['name 1'];
+
+// for(var i = 0; i<2;i++){
+//   guessedName = prompt('guess my name');
+//   namesArray.push(guessedName);
+// }
+
+
+// for(var index = 0; index < namesArray.length; index++){
+//   console.log(namesArray[index]);
+// }
+
+
+//logging numbers from 0-9 using while loop
+// var i = 0;
+// while(i < 10){
+//   console.log(i);
+//   i++;
+// }
+
+//keep asking user for input until a specific value is entered
+// var userInput = prompt('guess my name');
+// while(userInput !== 'dana'){
+//   userInput = prompt('guess my name');
+// }
+
+
+// do {
+//   guessedName = prompt('guess my name');
+//   console.log(guessedName);
+
+// } while(guessedName !== 'dana);
+
